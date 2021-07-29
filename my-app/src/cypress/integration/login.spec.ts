29daa40179a31caad32af9fc191bab6ff7b5616f
charleshardy1 <charlesgoffhardy@gmail.com>
2021-07-29 15:07:00 -0700

    add before each

--- my-app/src/cypress/integration/login.spec.ts
+++ my-app/src/cypress/integration/login.spec.ts
@@ -1,11 +1,10 @@
 /// <reference types="cypress" />
 
 describe('The Login Page', () => {
-  it('successfully loads', () => {
+  beforeEach(() => {
     cy.visit('http://localhost:19006/Login');
   });
   it('successfully rendered text', () => {
-    cy.visit('http://localhost:19006/Login');
     cy.get('[data-testid=username-input-label]').contains('Username:');
     cy.get('[data-testid=password-input-label]').contains('Password:');
   });
