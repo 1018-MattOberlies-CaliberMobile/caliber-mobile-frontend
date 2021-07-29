c191f59e56fd7c9da35c5c8188cbe356d65dc345
revix4 <james.p1@verizon.net>
2021-07-29 16:27:12 -0400

    Changed week-selector test to a tsx file

--- my-app/src/jest/week-selector.test.tsx
+++ my-app/src/jest/week-selector.test.tsx
@@ -1,7 +1,10 @@
 import { render, fireEvent } from '@testing-library/react-native';
+import React from 'react';
+import WeekNoteScreen from '../screens/WeekNotesScreen';
 
 describe('Week selector test', () => {
   test('Testing week change', () => {
+    const changeWeek = render(<WeekNoteScreen />);
     expect(1).toBe(1);
   });
 });
