b919546c8cd471a1b2e3e5939b711ba29b84303e
revix4 <james.p1@verizon.net>
2021-07-29 17:36:45 -0400

    Created positive and negative test case for the HorizontalSelector component

--- my-app/src/jest/week-selector.test.tsx
+++ my-app/src/jest/week-selector.test.tsx
@@ -1,10 +1,32 @@
 import { render, fireEvent } from '@testing-library/react-native';
+import 'react-native';
 import React from 'react';
-import WeekNoteScreen from '../screens/WeekNotesScreen';
+import HorizontalSelector from '../components/HorizontalSelector/HorizontalSelector';
 
-describe('Week selector test', () => {
-  test('Testing week change', () => {
-    const changeWeek = render(<WeekNoteScreen />);
-    expect(1).toBe(1);
+describe('Horizontal selector test', () => {
+  test('Testing pressing a button in the horizontal selector properly calls the onPress function', () => {
+    let output = '';
+    const setOutput = (item :string): void => { output = item; };
+
+    const { getByText } = render(
+      <HorizontalSelector data = {['week1', 'week2', 'week3']} onPress={ setOutput } />,
+    );
+
+    const week2Button = getByText('week2');
+    fireEvent.press(week2Button);
+    expect(output).toBe('week2');
+  });
+
+  test('Testing when a button is not pressed the onPress function is not being called', () => {
+    let output = '';
+    const setOutput = (item :string): void => { output = item; };
+
+    const { getByText } = render(
+      <HorizontalSelector data = {['week1', 'week2', 'week3']} onPress={ setOutput } />,
+    );
+
+    const week2Button = getByText('week2');
+
+    expect(output).not.toBe('week2');
   });
 });
