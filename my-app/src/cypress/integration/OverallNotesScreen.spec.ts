9a9b743fd01efde767e6e02876f2f2e8548192e4
dixond1097 <79214021+dixond1097@users.noreply.github.com>
2021-07-29 14:06:33 -0400

    Did testing for overall notes and status

--- my-app/src/cypress/integration/OverallNotesScreen.spec.ts
+++ my-app/src/cypress/integration/OverallNotesScreen.spec.ts
@@ -4,4 +4,22 @@ context('overall screen', () => {
   beforeEach(() => {
     cy.visit('http://localhost:19006/OverallNotes');
   });
+  context('week navigation', () => {
+    it('week navigation', () => {
+      cy.contains(/week [0-9]+/i);
+    });
+  });
+  context('overall week body', () => {
+    it('status is visible', () => {
+      cy.get('[data-testid="statusIndicator"]').click();
+      cy.get('[data-testid="statusSelector"]').should('be.visible');
+    });
+    it('notes visible', () => {
+      cy.get('[data-testid="overallNotesInput"]').type('text');
+      cy.get('[data-testid="overallNotesInput"]').invoke('val')
+        .then((text) => {
+          expect(text).to.equal('text');
+        });
+    });
+  });
 });
