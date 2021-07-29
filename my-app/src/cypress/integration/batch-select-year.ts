497d9b7285f1ab25e46324302432bf1bec481b52
realcryptogates <dksd93@gmail.com>
2021-07-30 03:07:06 +0900

    added data models

---
+++ my-app/src/cypress/integration/batch-select-year.ts
@@ -0,0 +1,7 @@
+/// <reference types="cypress" />
+
+context('year selection tests', () => {
+  beforeEach(() => {
+    cy.visit('http://localhost:19006/Root/HomeDrawer/QualityAudit/BatchSelection');
+  });
+});
