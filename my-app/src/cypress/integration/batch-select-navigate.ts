cf41ca119c6b152113befd6705dce5a896623b50
kirk simmonds <aldrich19@gmail.com>
2021-07-29 17:12:08 -0400

    cypress test for selecting a batch and navigating to the weeknotes page

--- my-app/src/cypress/integration/batch-select-navigate.ts
+++ my-app/src/cypress/integration/batch-select-navigate.ts
@@ -3,10 +3,21 @@
 context('Batch selection navigate test', () => {
   beforeEach(() => {
     cy.visit('http://localhost:19006/BatchSelection');
-    cy.get('[data-testid="batchName"]').click();
+    cy.fixture('../fixtures/batchData').then((json) => {
+      cy.intercept('GET', '/batch/time/*', json).as('getMockData');
+    });
   });
 });
 
 it('cy.go() - go forward to weeknotes page', () => {
+  cy.contains('05132020 Cloud Native Matt').click();
   cy.location('pathname').should('include', 'WeekNotes');
+  cy.window().its('store').invoke('getState').should('deep.equal', {
+    batchID: '123123',
+    batchTitle: '05132020 Cloud Native Matt',
+    trainerID: 'Matt',
+    startDate: '05/13/2020',
+    associateList: [],
+    notes: [],
+  });
 });
