2704724bfc28c6d3f96e4071340e8b82e3b27de6
dixond1097 <79214021+dixond1097@users.noreply.github.com>
2021-07-30 11:38:46 -0400

    Finished e2e testing for overall screen

---
+++ my-app/src/jest/overall-notes.test.tsx
@@ -0,0 +1,14 @@
+import { render } from "@testing-library/react-native";
+import React from "react";
+import HorizontalSelector from "../components/HorizontalSelector/HorizontalSelector";
+
+describe('overall testing page', () => {
+  test('Testing when a button is not pressed the onPress function is not being called', () => {
+    let output = '';
+    const setOutput = (item :string): void => { output = item; };
+
+    const { getByText } = render(
+      <HorizontalSelector data = {['week1', 'week2', 'week3']} onPress={ setOutput } />,
+    );
+  });
+});
