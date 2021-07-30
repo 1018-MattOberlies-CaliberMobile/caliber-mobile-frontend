8b9749b98ed81e3f00ac7c61cb61131b68f35172
zkao04 <hineskao@gmail.com>
2021-07-30 09:36:11 -0700

    worked on further end to end testing

--- my-app/src/cypress/integration/login.spec.ts
+++ my-app/src/cypress/integration/login.spec.ts
@@ -8,4 +8,18 @@ describe('The Login Page', () => {
     cy.get('[data-testid=username-input-label]').contains('Username:');
     cy.get('[data-testid=password-input-label]').contains('Password:');
   });
+  it('invalid login', () => {
+    cy.intercept({ method: 'Post' }, (req) => {
+      req.continue((res) => {
+        if (res.body.status === 'failed') {
+          expect(1).to.equal(1);
+        } else {
+          expect(2).to.equal(1);
+        }
+      });
+    });
+    cy.get('[data-testid=username-input]').type('invalid user');
+    cy.get('[data-testid=password-input]').type('wrong password');
+    cy.get('[data-testid=login-button]').click();
+  });
 });
