787eb63022faf5ec772b8ad4712323394e39b17f
dallinlemon <dallinlemon@gmail.com>
2021-07-30 13:11:25 -0600

    created unit tests in the overall notes tests

---
+++ my-app/src/components/StatusSelector.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+type Props = {
+  onSelect: () => void,
+}
+
+const StatusSelector: React.FC<Props> = (props): JSX.Element => {
+  const temp = 0;
+  return (
+    <>
+    </>
+  );
+};
+
+export default StatusSelector;
--- my-app/src/jest/overall-notes.test.tsx
+++ my-app/src/jest/overall-notes.test.tsx
@@ -1,8 +1,11 @@
-import { render } from '@testing-library/react-native';
+/* eslint-disable no-unused-expressions */
+import { fireEvent, GetAllReturn, render } from '@testing-library/react-native';
 import React from 'react';
-import HorizontalSelector from '../components/HorizontalSelector/HorizontalSelector';
+import { expect } from '@jest/globals';
+import HorizontalSelector from '../components/HorizontalSelector';
+import StatusSelector from '../components/StatusSelector';
 
-describe('overall testing page', () => {
+describe('Overall week note page tests', () => {
   test('Testing when a button is not pressed the onPress function is not being called', () => {
     let output = '';
     const setOutput = (item :string): void => { output = item; };
@@ -10,5 +13,75 @@ describe('overall testing page', () => {
     const { getByText } = render(
       <HorizontalSelector data = {['week1', 'week2', 'week3']} onPress={ setOutput } />,
     );
+
+    expect(getByText('week1')).toBeTruthy();
+  });
+
+  // status selector
+  // test onSelect is called with the correct info
+
+  test('On selecting the question mark calls onPress with returns 0', () => {
+    const mockFn = jest.fn();
+    const { getAllByA11yLabel } = render(
+      <StatusSelector onSelect={mockFn}/>,
+    );
+
+    const options: GetAllReturn = getAllByA11yLabel('statusOptions');
+
+    fireEvent.press(options[0]);
+
+    expect(mockFn).toBeCalledWith(0);
+  });
+
+  test('On selecting the poor option calls onPress with 1', () => {
+    const mockFn = jest.fn();
+    const { getAllByA11yLabel } = render(
+      <StatusSelector onSelect={mockFn}/>,
+    );
+
+    const options: GetAllReturn = getAllByA11yLabel('statusOptions');
+
+    fireEvent.press(options[1]);
+
+    expect(mockFn).toBeCalledWith(1);
+  });
+
+  test('On selecting the average option calls onPress with 2', () => {
+    const mockFn = jest.fn();
+    const { getAllByA11yLabel } = render(
+      <StatusSelector onSelect={mockFn}/>,
+    );
+
+    const options: GetAllReturn = getAllByA11yLabel('statusOptions');
+
+    fireEvent.press(options[2]);
+
+    expect(mockFn).toBeCalledWith(2);
+  });
+
+  test('On selecting the good option calls onPress with 3', () => {
+    const mockFn = jest.fn();
+    const { getAllByA11yLabel } = render(
+      <StatusSelector onSelect={mockFn}/>,
+    );
+
+    const options: GetAllReturn = getAllByA11yLabel('statusOptions');
+
+    fireEvent.press(options[3]);
+
+    expect(mockFn).toBeCalledWith(3);
+  });
+
+  test('On selecting the superstar option calls onPress with 4', () => {
+    const mockFn = jest.fn();
+    const { getAllByA11yLabel } = render(
+      <StatusSelector onSelect={mockFn}/>,
+    );
+
+    const options: GetAllReturn = getAllByA11yLabel('statusOptions');
+
+    fireEvent.press(options[4]);
+
+    expect(mockFn).toBeCalledWith(4);
   });
 });
