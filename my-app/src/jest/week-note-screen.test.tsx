49e778ef4d1131faf5d96abb601d9f5336e8764e
revix4 <james.p1@verizon.net>
2021-07-30 17:00:46 -0400

    Created tests for createWeekArray edge cases Created edge cases for createWeekArray

--- my-app/src/jest/week-note-screen.test.tsx
+++ my-app/src/jest/week-note-screen.test.tsx
@@ -1,7 +1,7 @@
 // eslint-disable-next-line import/no-extraneous-dependencies
 import { render, fireEvent } from '@testing-library/react-native';
+import { expect } from '@jest/globals';
 import 'react-native';
-import React from 'react';
 import { createWeekArray } from '../screens/WeekNotesScreen';
 
 describe('Testing week note screen', () => {
@@ -10,10 +10,12 @@ describe('Testing week note screen', () => {
     const weekOutput2 = createWeekArray('2021-7-5', '2021-7-19');
     const weekOutput3 = createWeekArray('2021-7-7', '2021-7-28');
     const weekOutput4 = createWeekArray('2021-7-30', '2021-7-30');
+    const weekOutput5 = createWeekArray('2021-7-8', '2021-7-27');
 
     expect(weekOutput).toStrictEqual(['week1', 'week2', 'week3', 'week4']);
-    expect(weekOutput2).toStrictEqual(['week1', 'week2']);
-    expect(weekOutput3).toStrictEqual(['week1', 'week2', 'week3']);
+    expect(weekOutput2).toStrictEqual(['week1', 'week2', 'week3']);
+    expect(weekOutput3).toStrictEqual(['week1', 'week2', 'week3', 'week4']);
     expect(weekOutput4).toStrictEqual([]);
+    expect(weekOutput5).toStrictEqual(['week1', 'week2', 'week3', 'week4']);
   });
 });
--- my-app/src/screens/WeekNotesScreen.tsx
+++ my-app/src/screens/WeekNotesScreen.tsx
@@ -9,9 +9,26 @@ export const createWeekArray = (start: string, end: string): string[] => {
   const MILLISECONDS_IN_WEEK = 7 * 24 * 60 * 60 * 1000;
   const startDate = new Date(start);
   const endDate = new Date(end);
+  let weekNum = 1;
+
+  if (startDate.getDate() === endDate.getDate()) {
+    return [];
+  }
+
+  // Edge case for when start and end date are the same day of the week
+  if (startDate.getDay() >= endDate.getDay()) {
+    // Monday is represented by 1
+    if (endDate.getDay() === 1) {
+      endDate.setDate(endDate.getDate() + 1);
+    } else {
+      weekArray.push(`week${weekNum}`);
+      weekNum += 1;
+      startDate.setDate(startDate.getDate() + ((1 + 7 - startDate.getDay()) % 7));
+    }
+  }
+
   const timeLength = endDate.valueOf() - startDate.valueOf();
   let timeRemaining = timeLength;
-  let weekNum = 1;
 
   while (timeRemaining > 0) {
     weekArray.push(`week${weekNum}`);
