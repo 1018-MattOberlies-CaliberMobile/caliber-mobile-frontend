aee4657c7c399d96a615e911b02dbd213e241d90
Sam Henley <samueljhenley@gmail.com>
2021-07-30 16:59:44 -0400

    install axios, begin developing API requests

--- my-app/src/remote/CaliberBatchAPI.ts
+++ my-app/src/remote/CaliberBatchAPI.ts
@@ -1,14 +1,22 @@
+/* eslint-disable arrow-body-style */
 /* eslint-disable import/prefer-default-export */
-
+import BackendClient from './CaliberBackendClient';
 import Batch from '../models/batch';
 
 /**
  * Retrieves a list of years which contain batches
  */
-export function getBatchYears(): string[] {
-  // TODO: replace mock data with API call
-  return ['2021', '2020', '2019', '2018', '2017', '2016', '2015', '2014', '2013', '2012', '2010', '2009'];
-}
+export const getBatchYears = async (): Promise<string[]> => {
+  return BackendClient.get<string[]>('/year')
+    .then((res) => {
+      console.log('Successfuly retrieved years', res.data);
+      return res.data as string[];
+    }).catch((err) => {
+      // TODO: add toast message
+      console.log('Error retrieving list of years: ', err);
+      return [];
+    });
+};
 
 /**
  * Retrieve a list of batches for the specified year
@@ -16,62 +24,14 @@ export function getBatchYears(): string[] {
  * batches that the user is a trainer of
  * @param year the year to retrieve batches for
  */
-export function getBatchesByYear(year: string): Batch[] {
-  // TODO: replace mock data with API call
-  console.log(year);
-  const DATA:Array<Batch> = [
-    {
-      batchId: '123123',
-      batchTitle: `0513${year} Cloud Native Matt`,
-      trainers: [
-        {
-          username: 'Matt',
-          role: 'Trainer',
-        },
-      ],
-      startDate: new Date().toISOString(),
-      endDate: new Date().toISOString(),
-      associates: [
-      ],
-      notes: [
-      ],
-    },
-    {
-      batchId: 'tryhtry',
-      batchTitle: `0513${year} DevOps Tim`,
-      trainers: [
-        {
-          username: 'Tim',
-          role: 'Trainer',
-        },
-      ],
-      startDate: new Date().toISOString(),
-      endDate: new Date().toISOString(),
-      associates: [
-      ],
-      notes: [
-      ],
-    },
-  ];
-  for (let i = 0; i < 20; i += 1) {
-    const newBatch: Batch = {
-      batchId: `${i}`,
-      batchTitle: `0513${year} ${i % 2 === 0 ? 'Cloud Native Matt' : 'DevOps Tim'}`,
-      trainers: [
-        {
-          username: `${i % 2 === 0 ? 'Matt' : 'Tim'}`,
-          role: 'Trainer',
-        },
-      ],
-      startDate: new Date().toISOString(),
-      endDate: new Date().toISOString(),
-      associates: [
-      ],
-      notes: [
-      ],
-    };
-    DATA.push(newBatch);
-  }
-
-  return DATA;
+export function getBatchesByYear(year: string): Promise<Batch[]> {
+  return BackendClient.get<Batch[]>(`/year/${year}`)
+    .then((res) => {
+      console.log('Successfuly retrieved batches', res.data);
+      return res.data as Batch[];
+    }).catch((err) => {
+      // TODO: add toast message
+      console.log('Error retrieving list of batches: ', err);
+      return [];
+    });
 }
