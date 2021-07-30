bbc7f0b723a1500216422c54f5edbe8f9dfc86de
Sam Henley <samueljhenley@gmail.com>
2021-07-30 12:02:10 -0400

    refactor Batch Selection Screen and components

---
+++ my-app/src/components/BatchCard.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Text, TouchableOpacity } from 'react-native';
+import Batch from '../models/batch';
+import { styles1 } from '../styles/style1';
+
+type Props = {
+  batch: Batch,
+  onPress: (batch: Batch) => void,
+}
+
+const BatchCard: React.FC<Props> = ({ batch, onPress }) => (
+  <TouchableOpacity onPress={(): void => onPress(batch)}style={styles1.item}>
+    <Text style={styles1.leftTxt}>Batch Name:  {batch.batchTitle} </Text>
+    <Text style={styles1.leftTxt}>Trainer:  {batch.trainers[0].userId} </Text>
+  </TouchableOpacity>
+);
+
+export default BatchCard;
