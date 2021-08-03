6350ae34b790d17c8e30379d9a07ebf5ab8ee0a7
dallinlemon <dallinlemon@gmail.com>
2021-08-03 12:47:10 -0600

    fixed jest external libraries error

--- jest.config.ts
+++ jest.config.ts
@@ -87,7 +87,9 @@ const config: InitialOptionsTsJest = {
   ],
 
   // A map from regular expressions to module names or to arrays of module names that allow to stub out resources with a single module
-  // moduleNameMapper: {},
+  moduleNameMapper: {
+    '\\.(jpg|ico|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$': '<rootDir>/__mocks__/fileMock.js',
+  },
 
   // An array of regexp pattern strings, matched against all module paths before considered 'visible' to the module loader
   // modulePathIgnorePatterns: [],
@@ -183,7 +185,7 @@ const config: InitialOptionsTsJest = {
   transformIgnorePatterns: [
     // '\\node_modules\\',
     //   "\\.pnp\\.[^\\\\]+$",
-    // 'node_modulses/(?!(@react-native|react-native|react-native-vector-icons|react-native-modal|react-native-animatable|@expo)/)',
+    'node_modulses/(?!(@react-native|react-native|react-native-vector-icons|react-native-modal|react-native-animatable|@expo)/)',
 
   ],
 
