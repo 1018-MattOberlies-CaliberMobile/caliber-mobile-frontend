f1b761312709ef44e8a666c18f41caf9c1bc3cc2
revix4 <james.p1@verizon.net>
2021-08-03 17:38:01 -0400

    Implemented Fisher Yates in place shuffle. Added Toggle Switch component.

--- src/remote/CaliberNoteAPI.ts
+++ src/remote/CaliberNoteAPI.ts
@@ -25,7 +25,15 @@ export const getNoteByBatchIdAndWeek = async (batchId: string, week: number): Pr
     notes.push({
       noteId: '123e4567-e89b-12d3-a456-4266141740123',
       noteContent: 'Very smart cookie!',
-      technicalScore: 2,
+      technicalScore: 4,
+      associate: { associateId: 'id', firstName: 'fn', lastName: 'ln' },
+      weekNumber: 1,
+    });
+
+    notes.push({
+      noteId: '123e4567-e89b-12d3-a456-4266141740456',
+      noteContent: 'Not so smart cookie!',
+      technicalScore: 1,
       associate: { associateId: 'id', firstName: 'fn', lastName: 'ln' },
       weekNumber: 1,
     });
@@ -36,6 +44,14 @@ export const getNoteByBatchIdAndWeek = async (batchId: string, week: number): Pr
       noteId: '123e4567-e89b-12d3-a456-426614174111',
       noteContent: 'Another note',
       technicalScore: 4,
+      associate: { associateId: 'id', firstName: 'fn', lastName: 'ln' },
+      weekNumber: 2,
+    });
+
+    notes.push({
+      noteId: '123e4567-e89b-12d3-a456-426614174222',
+      noteContent: 'Great job',
+      technicalScore: 3,
       associate: { associateId: 'id', firstName: 'fn', lastName: 'ln' },
       weekNumber: 2,
     });
