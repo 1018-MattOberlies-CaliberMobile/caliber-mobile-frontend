d02ecee83e83ea5dc028a73d6f851d2fcaf83b7a
Charles Ammons <ccaammon@gmail.com>
2021-08-04 14:45:40 -0500

    Updated with proper logic

--- src/components/AutoSelector.ts
+++ src/components/AutoSelector.ts
@@ -3,19 +3,19 @@ import { TechnicalScore } from '../@types';
 export default function AutoSelector(scoreArray: TechnicalScore[]): TechnicalScore {
   const zeroCount = (scoreArray.filter((score) => score === 0)).length;
   let realFinal = 0 as TechnicalScore;
-  if (zeroCount <= (0.2 * scoreArray.length)) {
+  if (scoreArray.length - zeroCount > 0) {
     let sum = 0;
     // eslint-disable-next-line no-plusplus
     for (let i = 0; i < scoreArray.length; i++) {
       sum += scoreArray[i];
     }
-    const average = sum / scoreArray.length;
+    const average = sum / (scoreArray.length - zeroCount);
     let overallDef = 0;
-    if (average >= 3.5) {
+    if (average === 4) {
       overallDef = 4;
-    } else if (average < 3.5 && average >= 2.5) {
+    } else if (average < 4 && average >= 2.5) {
       overallDef = 3;
-    } else if (average < 2.5 && average >= 1.5) {
+    } else if (average < 2.5 && average >= 2) {
       overallDef = 2;
     } else {
       overallDef = 1;
--- src/jest/auto-overall-status-selector.test.tsx
+++ src/jest/auto-overall-status-selector.test.tsx
@@ -4,16 +4,16 @@ import autoSelector from '../components/AutoSelector';
 
 describe('auto overall status selector', () => {
   it('picks an overall technical status based on individual scores', () => {
-    const sample1 = [1, 1, 2, 3, 4, 2, 0, 3, 1, 1, 2, 3, 0] as TechnicalScore[];
+    const sample1 = [1, 1, 1, 1, 1, 2, 0, 1, 1, 1, 2, 3, 0] as TechnicalScore[];
     const sample2 = [0, 0, 0, 0, 1, 2, 3] as TechnicalScore[];
-    const sample3 = [3, 4, 4, 4, 3, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4] as TechnicalScore[];
-    const sample4 = [1, 2, 1, 4, 1, 1, 1, 1, 1, 1, 2, 0, 0, 0, 1, 1, 1] as TechnicalScore[];
+    const sample3 = [4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4] as TechnicalScore[];
+    const sample4 = [0, 0, 0, 0, 0] as TechnicalScore[];
     const sample5 = [3, 1, 2, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 4, 1, 0] as TechnicalScore[];
 
-    expect(autoSelector(sample1)).toBe(2 as TechnicalScore);
-    expect(autoSelector(sample2)).toBe(0 as TechnicalScore);
+    expect(autoSelector(sample1)).toBe(1 as TechnicalScore);
+    expect(autoSelector(sample2)).toBe(2 as TechnicalScore);
     expect(autoSelector(sample3)).toBe(4 as TechnicalScore);
-    expect(autoSelector(sample4)).toBe(1 as TechnicalScore);
+    expect(autoSelector(sample4)).toBe(0 as TechnicalScore);
     expect(autoSelector(sample5)).toBe(3 as TechnicalScore);
   });
 });
