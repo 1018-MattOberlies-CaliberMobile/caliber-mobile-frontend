61931b0b0b8c719c0626799b50b02241443b03bf
Sam Henley <samueljhenley@gmail.com>
2021-08-04 11:53:32 -0400

    remove border from selector buttons

--- src/styles/HorizontalSelector.ts
+++ src/styles/HorizontalSelector.ts
@@ -2,12 +2,10 @@ import { StyleSheet } from 'react-native';
 
 const baseStyle = {
   borderRadius: 5,
-  borderWidth: 2,
   flex: 1,
   fontSize: 24,
   margin: 3,
   padding: 5,
-  borderColor: '#474C55',
 };
 
 const HorizontalSelectorStyle = StyleSheet.create({
