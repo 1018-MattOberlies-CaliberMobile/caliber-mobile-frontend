cac0b01c9a8e137ccf14a808e6bc45baa3d2278d
DustinDiazLopez <hunterghost526@gmail.com>
2021-08-04 22:16:44 -0500

    Making req to the backend

--- src/remote/CaliberNoteAPI.ts
+++ src/remote/CaliberNoteAPI.ts
@@ -16,6 +16,7 @@ export const getNoteByBatchIdAndWeek = async (batchId: string, week: number): Pr
   if (week === 1) {
     notes.push({
       noteId: '123e4567-e89b-12d3-a456-426614174000',
+      batchId,
       noteContent: 'A note',
       technicalScore: 0,
       associate: { associateId: 'id', firstName: 'fn', lastName: 'ln' },
@@ -24,6 +25,7 @@ export const getNoteByBatchIdAndWeek = async (batchId: string, week: number): Pr
 
     notes.push({
       noteId: '123e4567-e89b-12d3-a456-4266141740123',
+      batchId,
       noteContent: 'Very smart cookie!',
       technicalScore: 4,
       associate: { associateId: 'id', firstName: 'fn', lastName: 'ln' },
@@ -32,6 +34,7 @@ export const getNoteByBatchIdAndWeek = async (batchId: string, week: number): Pr
 
     notes.push({
       noteId: '123e4567-e89b-12d3-a456-4266141740456',
+      batchId,
       noteContent: 'Not so smart cookie!',
       technicalScore: 1,
       associate: { associateId: 'id', firstName: 'fn', lastName: 'ln' },
@@ -42,6 +45,7 @@ export const getNoteByBatchIdAndWeek = async (batchId: string, week: number): Pr
   if (week === 2) {
     notes.push({
       noteId: '123e4567-e89b-12d3-a456-426614174111',
+      batchId,
       noteContent: 'Another note',
       technicalScore: 4,
       associate: { associateId: 'id', firstName: 'fn', lastName: 'ln' },
@@ -50,6 +54,7 @@ export const getNoteByBatchIdAndWeek = async (batchId: string, week: number): Pr
 
     notes.push({
       noteId: '123e4567-e89b-12d3-a456-426614174222',
+      batchId,
       noteContent: 'Great job',
       technicalScore: 3,
       associate: { associateId: 'id', firstName: 'fn', lastName: 'ln' },
@@ -61,7 +66,7 @@ export const getNoteByBatchIdAndWeek = async (batchId: string, week: number): Pr
 };
 
 export const CreateOverallNote = (note: Note): void => {
-  BackendClient.post('/note', note)
-    .then((res) => { console.log('>> Saved note', res); })
-    .catch((err) => { console.log('>> Error on save note.', err); });
+  BackendClient.post('note', note)
+    .then((res) => { console.log('>> Saved overall note', res); })
+    .catch((err) => { console.error('>> Error on save overall note.', err); });
 };
