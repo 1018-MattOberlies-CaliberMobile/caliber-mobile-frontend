b6e09156651e65bb27f99a67a636c4119c21e784
Dustin Díaz <hunterghost526@gmail.com>
2021-08-06 17:12:43 -0500

    Ok cool beans (#53)
    * created custom font
    
    * Created Pressable Model
    
    * fixed jest external libraries error
    
    * fixed overallNotes unit testing
    
    * added functionality to the create over all note page
    
    * removed duplicate functions
    
    * Finished styling on overall notes page
    
    * Making req to the backend
    
    * merged in overall changes
    
    * merged in overall tests
    
    * fixed jest external libraries error
    
    * merged in overall screen
    
    * merged in overall screen and updated redux
    
    * merged in overall screen
    
    * Finished overall notes screen
    
    * changed backend endpoint
    
    Co-authored-by: dallinlemon <dallinlemon@gmail.com>
    Co-authored-by: dixond1097 <79214021+dixond1097@users.noreply.github.com>
    Co-authored-by: dallinlemon <84731544+dallinlemon@users.noreply.github.com>

--- src/remote/CaliberBackendClient.ts
+++ src/remote/CaliberBackendClient.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const BACKEND_URL = 'https://qm2m9ri1ua.execute-api.us-east-1.amazonaws.com/dev/api/v1/';
+const BACKEND_URL = 'https://k9ussq7588.execute-api.us-east-1.amazonaws.com/dev/api/v1/';
 
 const BackendClient = axios.create({
   baseURL: BACKEND_URL,
