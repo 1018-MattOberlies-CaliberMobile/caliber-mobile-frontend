29eb8fa9c43ce5ed0af73cefeb7b5b9b7f60fe3f
Charles-Ammons <80184931+Charles-Ammons@users.noreply.github.com>
2021-08-10 18:18:55 -0500

    Auto tech stat (#60)
    * add optional initialSelected prop to horizontal selector
    
    * move HorizontalSelector out of its own directory
    
    * refactor Batch Selection Screen and components
    
    * refactor userId to username
    
    * add batch slice to redux
    
    * rename getBatch to selectBatch to be consistent with user store
    
    * move mock data to mock API call
    
    * on selecting a batch, set redux store to be that batch and navigate to notes page
    
    * install axios, begin developing API requests
    
    * consolidate batch selection tests
    
    * finish writing e2e tests for Batch Select Page
    
    * add toast messages to getBatchYears and getBatchesByYear
    
    * setup cognito
    
    * config cognito
    
    * config cognito
    
    * Implemented cognito to Login Page
    
    * Created addCognitoUsers.ts to add cognito users, works
    
    * remove single quotes from 'role' in addCognitoUser
    
    * sort batch list in descending order
    
    * finish merge conflicts
    
    * tested login functionality
    
    * work on styles
    
    * batch slice stores object as JSON string
    
    * remove border from selector buttons
    
    * remove debugging console.log
    
    * Added AutoSelect Function
    
    * horizontal selector uses revature font
    
    * style search bar
    
    * Updated with proper logic
    
    * add cognito auth to getBatchesByYear
    
    * Updated to accpet note array
    
    * dispatch to store sends a JSON of batch
    
    * move search bar styles to more descriptive file name
    
    * finish batch selection page functionality
    
    * fix merge conflicts
    
    * Added auto-select on Overall Note Comp
    
    * fix batch object received from backend
    
    * remove unnecessary import
    
    * No real progress, just an update
    
    * On an odyssey
    
    * styles for batch card and search button
    
    * try to fix cypress test for batch selection
    
    * search bar text input styles
    
    * list batch years in ascending order
    
    * fix error in search bar finding trainer username
    
    * fix casing in import
    
    * Removed attempted implementation. Maybe just merge function for future generations?
    
    * Systematically eliminated every change except AutoSelector
    
    * Put the amplify thing back
    
    * Still attempting to correct amplify
    
    Co-authored-by: Sam Henley <samueljhenley@gmail.com>
    Co-authored-by: charleshardy1 <charlesgoffhardy@gmail.com>
    Co-authored-by: zkao04 <hineskao@gmail.com>
    Co-authored-by: David Quezada <dvd.quezada@gmail.com>
    Co-authored-by: dallinlemon <84731544+dallinlemon@users.noreply.github.com>

---
+++ src/jest/auto-overall-status-selector.test.tsx
@@ -0,0 +1,70 @@
+import { expect } from '@jest/globals';
+import { TechnicalScore } from '../@types';
+import autoSelector from '../functions/AutoSelector';
+import Associate from '../models/associate';
+import Note from '../models/note';
+
+describe('auto overall status selector', () => {
+  it('picks an overall technical status based on individual scores', () => {
+    const sampleAssociate: Associate = {
+      associateId: 'test',
+      firstName: 'sample',
+      lastName: 'associate',
+    };
+
+    const note0: Note = {
+      noteId: 'Test0',
+      noteContent: 'string',
+      technicalScore: 0 as TechnicalScore,
+      weekNumber: 5,
+      associate: sampleAssociate,
+    };
+
+    const note1: Note = {
+      noteId: 'Test1',
+      noteContent: 'string',
+      technicalScore: 1 as TechnicalScore,
+      weekNumber: 5,
+      associate: sampleAssociate,
+    };
+
+    const note2: Note = {
+      noteId: 'Test2',
+      noteContent: 'string',
+      technicalScore: 2 as TechnicalScore,
+      weekNumber: 5,
+      associate: sampleAssociate,
+    };
+
+    const note3: Note = {
+      noteId: 'Test3',
+      noteContent: 'string',
+      technicalScore: 3 as TechnicalScore,
+      weekNumber: 5,
+      associate: sampleAssociate,
+    };
+
+    const note4: Note = {
+      noteId: 'Test0',
+      noteContent: 'string',
+      technicalScore: 4 as TechnicalScore,
+      weekNumber: 5,
+      associate: sampleAssociate,
+    };
+
+    const sample1 = [note1, note1, note1, note1, note1, note2, note0,
+      note1, note1, note1, note2, note3, note0] as Note[];
+    const sample2 = [note0, note0, note0, note0, note1, note2, note3] as Note[];
+    const sample3 = [note4, note4, note4, note4, note4, note4, note4,
+      note4, note4, note4, note4, note4, note4, note4, note4] as Note[];
+    const sample4 = [note0, note0, note0, note0, note0] as Note[];
+    const sample5 = [note3, note1, note2, note3, note3, note3, note3,
+      note3, note3, note3, note3, note3, note3, note3, note4, note1, note0] as Note[];
+
+    expect(autoSelector(sample1)).toBe(1 as TechnicalScore);
+    expect(autoSelector(sample2)).toBe(2 as TechnicalScore);
+    expect(autoSelector(sample3)).toBe(4 as TechnicalScore);
+    expect(autoSelector(sample4)).toBe(0 as TechnicalScore);
+    expect(autoSelector(sample5)).toBe(3 as TechnicalScore);
+  });
+});
